7f9db0bd60dcfd81fcbc2a14da41d8057091a16e
felden <michael.serra1@outlook.com>
2025-01-13 19:49:13 -0800

    inital nix config

---
+++ vim.nix
@@ -0,0 +1,49 @@
+{ pkgs, ... }:
+
+{
+	environment.variables = { EDITOR = "vim"; };
+	environment.systemPackages = with pkgs; [
+
+		((vim_configurable.override { }).customize{
+		 name = "vim";
+
+		 vimrcConfig.packages.myPlugins = with pkgs.vimPlugins; {
+		 start = [
+		 ale
+		 rust-vim
+		 YouCompleteMe
+		 auto-pairs
+		 syntastic
+		 ];
+
+		 opt = [ ];
+
+		 };
+
+		 vimrcConfig.customRC = ''
+		 syntax enable
+		 colorscheme slate
+		 set tabstop=4
+		 set shiftwidth=4
+		 set expandtab
+		 set hlsearch
+		 set incsearch
+		 set ignorecase
+		 set smartcase
+		 set showmatch
+		 set ruler
+		 set wrap
+		 set relativenumber
+		 set scrolloff=5
+		 filetype plugin indent on
+		 highlight LineNr ctermfg=grey
+         highlight MatchParen ctermbg=242 guibg=#6c6c6c
+         highlight matchparen cterm=underline gui=underline ctermfg=NONE guifg=NONE ctermbg=NONE guibg=NONE
+		 inoremap jj <Esc>
+		 autocmd CmdlineLeave /,\? :set nohlsearch
+		 '';
+
+		}
+
+	)];
+}
